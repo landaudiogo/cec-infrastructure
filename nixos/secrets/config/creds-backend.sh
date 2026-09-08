a8935db4f4e10eeb2ffc15aaac37a4553722d795
landaudiogo <diogo.hewitt.landau@hotmail.com>
2026-09-08 18:59:07 +0200

    creds: Deploy backend

--- nixos/secrets/config/creds-backend.sh
+++ nixos/secrets/config/creds-backend.sh
@@ -5,7 +5,7 @@ set -e
 script_d="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
 decrypted_d="${script_d}/../.decrypted"
 
-ADMIN_UUID="$(uuidgen | base64)"
+ADMIN_UUID="$(uuidgen | tr -d '\n' | base64)"
 JWT_SECRET="$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ' | base64)"
 
 echo '{
